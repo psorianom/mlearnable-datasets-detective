ae55d79d238745dc4f1db5be3866986bc593ab70
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-06-17 13:59:06 +0200

    fixed the path issue for realz

--- src/models/dabl_dgf.py
+++ src/models/dabl_dgf.py
@@ -30,7 +30,8 @@ np.random.seed(42)
 
 def run(csv_file_path, csv_detective_cache):
     tqdm.write(f"Treating {csv_file_path} file")
-    csv_id = Path(csv_file_path).stem
+    csv_file_path = Path(csv_file_path)
+    csv_id = csv_file_path.stem
     results_list = []
     csv_metadata = get_csv_detective_metadata(csv_detective_cache=csv_detective_cache, csv_file_path=csv_file_path)
     if csv_metadata and len(csv_metadata) > 1:
@@ -111,15 +112,15 @@ def get_csv_detective_metadata(csv_detective_cache: dict, csv_file_path: Path, n
     return dict_result
 
 
-def main(csv_path: Path, n_jobs: int, csv_detective_json: Path):
+def main(csv_file_path: Path, n_jobs: int, csv_detective_json: Path):
     csv_detective_cache = load_csv_detective_cache(csv_detective_json=csv_detective_json) or {}
     list_files = []
 
-    if csv_path.exists():
-        if csv_path.is_file():
-            list_files = [csv_path]
+    if csv_file_path.exists():
+        if csv_file_path.is_file():
+            list_files = [csv_file_path]
         else:
-            list_files = glob.glob(csv_path.as_posix() + "/**/*.csv", recursive=True)
+            list_files = glob.glob(csv_file_path.as_posix() + "/**/*.csv", recursive=True)
 
     if n_jobs < 2:
         job_output = []
