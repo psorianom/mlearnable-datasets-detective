97688ae087d67a6cd4001d106c86d78c9862ee33
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-06-17 13:29:05 +0200

    dabling the datasets

--- src/models/dabl_dgf.py
+++ src/models/dabl_dgf.py
@@ -9,7 +9,11 @@ Arguments:
     --csv_detective_json FILE          A JSON file with the analysis of a csv_detective run over multiple CSVs [default: None:str]
     --num_cores=<n> CORES              Number of cores to use [default: 1:int]
 '''
+from datetime import datetime
+
+today = datetime.today().strftime('%d_%m_%Y')
 import glob
+from collections import defaultdict
 from pathlib import Path
 
 import dabl
@@ -25,24 +29,45 @@ np.random.seed(42)
 
 
 def run(csv_file_path, csv_detective_cache):
+    csv_id = Path(csv_file_path).stem
+    results_list = []
     csv_metadata = get_csv_detective_metadata(csv_detective_cache=csv_detective_cache, csv_file_path=csv_file_path)
     if csv_metadata and len(csv_metadata) > 1:
         encoding = csv_metadata["encoding"]
         sep = csv_metadata["separator"]
     else:
-        encoding = "latin-9"
+        encoding = "latin-9"  # bcause why not
         sep = ";"
+    csv_detective_columns = [k.strip('"') for k, v in csv_metadata['columns'].items() if "booleen" not in v]
 
     data: pd.DataFrame = pd.read_csv(csv_file_path.as_posix(), encoding=encoding, sep=sep)
+    # remove csv_detective columns
+    data = data.drop(csv_detective_columns, axis=1)
     data_clean, data_types = dabl.clean(data, return_types=True, verbose=3)
     # dabl.detect_types(data)
-    categorical_variables = data_types[data_types['categorical'] == True].index.values
-    for cat_var in categorical_variables:
-        print(f"Building models with target variable: {cat_var}")
-        sc = dabl.SimpleClassifier(random_state=42).fit(data_clean, target_col=cat_var)
-        print(sc.__dict__)
-        dabl.explain(sc)
-    pass
+    categorical_variables = data_types[data_types['categorical']].index.values
+    for target_col in categorical_variables:
+        try:
+            if data_clean[target_col].isna().sum():
+                continue
+            print(f"Building models with target variable: {target_col}")
+            sc = dabl.SimpleClassifier(random_state=42).fit(data, target_col=target_col)
+            inner_dict = {"csv_id": csv_id, "task": "classification",
+                          "algorithm": sc.current_best_.name,
+                          "target_col": target_col,
+                          "features_names": "|".join(sc.feature_names_),
+                          "date": today,
+                          "nb_classes": len(data[target_col].unique())
+                          }
+            inner_dict.update(sc.current_best_.to_dict())
+            inner_dict.update({"avg_scores": np.mean(list(sc.current_best_.to_dict().values()))})
+            results_list.append(inner_dict)
+        except Exception as e:
+            tqdm.write(f"Could not analyse file {csv_id} with target col {target_col}. Error {str(e)}")
+            results_list.append(None)
+
+        # dabl.explain(sc)
+    return results_list
 
 
 def load_csv_detective_cache(csv_detective_json: Path):
@@ -76,7 +101,7 @@ def get_csv_detective_metadata(csv_detective_cache: dict, csv_file_path: Path, n
 
     dict_result = routine(csv_file_path.as_posix(), num_rows=num_rows)
     csv_detective_cache[csv_id] = dict_result
-    json.dump(csv_detective_cache, open("./data/csv_detective_analysis.json", "w"))
+    json.dump(csv_detective_cache, open("./data/csv_detective_analysis.json", "w"), indent=4)
     return dict_result
 
 
@@ -92,11 +117,23 @@ def main(csv_path: Path, n_jobs: int, csv_detective_json: Path):
 
     if n_jobs < 2:
         job_output = []
+        dabl_analyzed_count = 0
+        csv_analyzed_total = 0
         for csv_file_path in tqdm(list_files):
-            job_output.append(run(csv_file_path, csv_detective_cache))
+            csv_analyzed_total += 1
+            dabl_result = run(csv_file_path, csv_detective_cache)
+            if all(v is None for v in dabl_result):  # all contents are None
+                continue
+            dabl_analyzed_count += 1
+            job_output.extend(dabl_result)
+
     else:
         job_output = Parallel(n_jobs=n_jobs)(
             delayed(run)(csv_file_path, csv_detective_cache) for csv_file_path in tqdm(list_files))
+
+    json.dump(job_output, open("./data/dabl_analysis.json", "w"), indent=4)
+    tqdm.write(f"We tried {csv_analyzed_total} csv files, we could do at least one dabl model in {dabl_analyzed_count}"
+               f" files.")
 
 
 if __name__ == '__main__':
